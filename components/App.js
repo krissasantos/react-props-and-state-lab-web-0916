0a4206f3a683737a6d0e930e1b77bf84ebb7a706
Flatiron School <flatironschool@Drew-2.local>
2016-11-18 13:17:15 -0500

    Pets mostly completed

--- components/App.js
+++ components/App.js
@@ -37,4 +37,13 @@ class App extends React.Component {
   }
 }
 
+// const MALE_DOG = {
+//   "id": "9e7cc723-d7f5-440d-8ead-c311e68014ee",
+//   "type": "dog",
+//   "gender": "male",
+//   "age": 8,
+//   "weight": 6,
+//   "name": "Kennedy"
+// };
+
 module.exports = App;
--- components/PetBrowser.js
+++ components/PetBrowser.js
@@ -6,10 +6,19 @@ class PetBrowser extends React.Component {
   render() {
     return (
       <div className="ui cards">
-        <code>&lt;Pet /&gt;</code> &nbsp; components should go here
+        <Pet pet={MALE_DOG} isAdopted={false}/>
       </div>
     );
   }
 }
 
+const MALE_DOG = {
+  "id": "9e7cc723-d7f5-440d-8ead-c311e68014ee",
+  "type": "dog",
+  "gender": "male",
+  "age": 8,
+  "weight": 6,
+  "name": "Kennedy",
+
+};
 module.exports = PetBrowser;
